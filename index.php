397414296412b36ce4894e8f7d04a64719dcfafe
rehaanhan <raihanmuabrok811@gmai.com>
2024-06-01 10:58:32 +0700

    fix function

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
           <li><a href="#gallery">Gallery</a></li>
           <li><a href="#sponsors">Sponsors</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li class="buy-tickets"><a href="#buy-tickets">Registration</a></li>
+          <li class="buy-tickets"><a href="regist.php">Registration</a></li>
         </ul>
       </nav><!-- #nav-menu-container -->
     </div>
@@ -71,7 +71,7 @@
   ============================-->
   <section id="intro">
     <div class="intro-container wow fadeIn">
-      <h1 class="mb-4 pb-0">The Annual<br><span>Marketing</span> Conference</h1>
+      <h1 class="mb-4 pb-0">International<br><span>Accuntantion</span> Conference</h1>
       <p class="mb-4 pb-0">10-12 December, Muhammadiyah Bandung University, Bandung</p>
       <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video"
         data-autoplay="true"></a>
@@ -86,21 +86,10 @@
     ============================-->
     <section id="about">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <h2>About The Event</h2>
-            <p>Sed nam ut dolor qui repellendus iusto odit. Possimus inventore eveniet accusamus error amet eius aut
-              accusantium et. Non odit consequatur repudiandae sequi ea odio molestiae. Enim possimus sunt inventore in
-              est ut optio sequi unde.</p>
-          </div>
-          <div class="col-lg-3">
-            <h3>Where</h3>
-            <p>Muhammadiyah Bandung University, Bandung</p>
-          </div>
-          <div class="col-lg-3">
-            <h3>When</h3>
-            <p>Monday to Wednesday<br>10-12 December</p>
-          </div>
+        <div class="aboutnav">
+          <div class="navigasion">Register</div>
+          <div class="navigasion">Login</div>
+          <div class="navigasion">schedule</div>
         </div>
       </div>
     </section>
--- regist.php
+++ regist.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-echo "Koneksi berhasil!";
+
 ?>
 
 
